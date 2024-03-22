3fbe9673e13faa063d9cd7ee597d20c0e4a7a685
João Pedro R. Diniz <jp_rd@hotmail.com>
2024-03-22 11:14:17 -0300

    Index.js - Corrigido e atualizado

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const server = express()
 const cors = require('cors')
-
-server.set(cors())
-server.set(express.json())
-
-const conn = require('./db/conn')
-conn()
-
+const connectDB = require('./db/connection')
 const routes = require('./routes/router')
+
+server.use(cors())
+server.use(express.json())
 server.use('/api', routes)
 
-server.listen(3000, () => {
+const USER_DB = process.env.userdb || 'root'
+const PASS_DB = process.env.passdb || 'example'
+const HOST_DB = process.env.hostdb || 'localhost'
+const PORT_DB = process.env.portdb || 27017
+
+const PORT = process.env.port || 3000
+
+connectDB(USER_DB, PASS_DB, HOST_DB, PORT_DB)
+
+server.listen(PORT, () => {
     console.log('Server online!')
 })
